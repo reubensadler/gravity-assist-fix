49224a33c487e14d2638d6f6bb1554b1e31e8269
reubensadler <57753204+reubensadler@users.noreply.github.com>
2020-01-21 14:49:12 +0000

    Improved readability and removed redundant code

--- gravityAssistFix.cpp
+++ gravityAssistFix.cpp
@@ -3,80 +3,89 @@
 #include <sstream>
 #include <vector>
 #include <string>
+#include <vector>
 
-int main()
+void fillArray(std::vector<int>& array)
 {
-	int array[999] = { 0 };
-	int tempArray[999];
-	std::copy(std::begin(array), std::end(array), std::begin(tempArray));
-	int i{ 0 };
-
-	std::cout << "input data: ";
-	while (i < 153)
+	/* std::cout << "Input data: ";
+	int value{};
+	while (std::cin >> value)		//put all data into the array
 	{
-		std::cin >> array[i];
-		i++;
+		array.push_back(value);		//put current value at the end of the array
+	} */
+
+	std::cout << "Input data: ";
+	int value{};
+	while (std::cin >> value)		//put all data into the array
+	{
+		if (!std::cin.peek())
+			break;
+		array.push_back(value);		//put current value at the end of the array
 	}
+}
+
+int determineInput(const std::vector<int>& array)
+{
+	std::vector<int> tempArray;
+
+	int addressMax{ static_cast<int>(array.size()) };
+
+	//the "noun" is the value in address 1, the "verb" is the value in address 2
+	//required to determine the noun and verb to complete the task
 	int noun{ 0 };
 	int verb{ 0 };
 
-	for(noun; noun < 100; noun++)
+	//try all combinations of nouns and verbs until the program outputs 19690720
+	for (noun; noun < 100; noun++)
 	{
 		verb = 0;
 		for (verb; verb < 100; verb++)
 		{
-			std::cout << noun << " , " << verb << '\n';
-			std::copy(std::begin(array), std::end(array), std::begin(tempArray));
-			i = 0;
-			tempArray[1] = noun;
+			int addressCount{ 0 };	//every iteration starts at address 0
+			tempArray = array;		//the following code will alter the values in tempArray, so reset for each iteration
+			tempArray[1] = noun;	//insert the noun and verb into code
 			tempArray[2] = verb;
-			int opCode{};
-			int address1{};
-			int address2{};
-			int address3{};
-			while (i < 153)
+			
+			for (addressCount; addressCount + 3 < addressMax; addressCount += 4)
 			{
-				opCode = tempArray[i];
-				if (tempArray[i] == 1)
-				{
-					address1 = tempArray[i + 1];
-					address2 = tempArray[i + 2];
-					address3 = tempArray[i + 3];
-					if (address3 > 153)
-					{
-						i=153;
-					}
-					else
-						tempArray[address3] = tempArray[address1] + tempArray[address2];
-					//std::cout << "(sum) array" << x << " = " << array[x] << '\n';
+				int opCode{ tempArray[addressCount] };	//determines what action to take with three successive values (parameters)
+				int parameter1{ tempArray[addressCount + 1] };
+				int parameter2{ tempArray[addressCount + 2] };
+				int parameter3{ tempArray[addressCount + 3] };
 
-				}
-				if (tempArray[i] == 2)
-				{
-					address1 = tempArray[i + 1];
-					address2 = tempArray[i + 2];
-					address3 = tempArray[i + 3];
-					if (address3 > 153)
-					{
-						i=153;
-					}
-					else
-						tempArray[address3] = tempArray[address1] * tempArray[address2];
-					//std::cout << "(product) array" << x << " = " << array[x] << '\n';
-				}
-				if (tempArray[i] == 99)
-				{
-					if (tempArray[0] == 19690720)
-					{
-						std::cout << "output: " << tempArray[0] << '\n';
+				switch (opCode) {
+					case (1) :
+						tempArray[parameter3] = tempArray[parameter1] + tempArray[parameter2];
+						break;
+
+					case (2) :
+						tempArray[parameter3] = tempArray[parameter1] * tempArray[parameter2];
+						break;
+
+					case (99) :
+						if (tempArray[0] == 19690720)
+						{
+							std::cout << "\nFound input noun and verb that output 19690720. Noun is " << noun << " and verb is " << verb << '\n';
+							return 0;
+						}
+						break;
+					default :
+						std::cout << "\nEncountered invalid opcode. Only opcodes 1, 2 and 99 are allowed. Check code and input data\n";
 						return 0;
-					}
 				}
-				i = i + 4; 
 			}
 		}
 	}
+	std::cout << "\nSolution not found. Check code and input data. \n";
+	return 0;
+}
 
+int main()
+{
+	std::vector<int> array;
+	fillArray(array);
+	
+	determineInput(array);
 
 	return 0;
 }
